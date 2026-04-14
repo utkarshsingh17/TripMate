addeceef3d2224268f77e82a97eb0fe99f417db4
utkarsh-dev <utkarsh@zooper.ai>
2026-04-14 17:21:20 +0530

    initial commit

---
+++ src/main/java/com/utkarsh/tripmate/TripmateApplication.java
@@ -0,0 +1,13 @@
+package com.utkarsh.tripmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utkarsh/tripmate/TripmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.tripmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
